9032bd17ec78eb79e266eeb3c975fc27fe227c41
Vadim Budarin <budarin-vv@yandex.ru>
2023-05-07 22:54:53 +0300

    fix code

--- src/client/ui/NavPanel/CategoryHeader/index.tsx
+++ src/client/ui/NavPanel/CategoryHeader/index.tsx
@@ -23,21 +23,21 @@ const CategoryHeader = memo(({ onCreateCategory }: CategoryHeader): JSX.Element 
                         <svg className="category-header-button-icon" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                             />
                             <path
                                 d="M 8 14 L 11.962 10.002"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                             />
                             <path
                                 d="M 12 10.002 L 15.962 14"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
@@ -47,21 +47,21 @@ const CategoryHeader = memo(({ onCreateCategory }: CategoryHeader): JSX.Element 
                         <svg className="category-header-button-icon" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                             />
                             <path
                                 d="M8 12H16"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                             />
                             <path
                                 d="M12 16V8"
-                                stroke="#292D32"
+                                stroke="currentColor"
                                 strokeWidth="1.5"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
