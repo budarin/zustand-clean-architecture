847433ac011958c4aed5cf8e32759d788a71bd11
vadim_budarin <budarin-vv@yandex.ru>
2023-05-16 18:45:38 +0300

    fix

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,11 +2,3 @@
 
 set -e
 
-hasChanges=$(yarn outdated --silent)
-
-if [ -n "$hasChanges" ]; then
-  echo "Обнаружены изменения в зависимостях. Выполняется установка..."
-  yarn install
-else
-  echo "Изменений в зависимостях не обнаружено."
-fi
