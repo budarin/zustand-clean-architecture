0781ac4c1af8031866c5f0e15b0d5f31789d65b6
vadim_budarin <budarin-vv@yandex.ru>
2023-05-28 23:02:29 +0300

    Bug: календарь не отрисовывает последний день календаря

--- src/common/utils/dateTime/getDatesDiffInDays.ts
+++ src/common/utils/dateTime/getDatesDiffInDays.ts
@@ -3,5 +3,5 @@ const ONE_DAY = 1000 * 60 * 60 * 24;
 export function getDatesDiffInDays(date1: Date, date2: Date) {
     const diffInMilliseconds = date2.getTime() - date1.getTime();
 
-    return Math.ceil(diffInMilliseconds / ONE_DAY);
+    return Math.ceil(diffInMilliseconds / ONE_DAY) + 1;
 }
