ec839c24d046ed3fdc2f42fe13dba5a838548aab
Vadim Budarin <budarin-vv@yandex.ru>
2023-06-01 17:16:32 +0300

    fix code

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,7 +9,6 @@ const todosUrl = '/api/get_todos';
 const jsonHeader = { 'Content-Type': 'application/json; charset=utf-8' };
 
 let state: Entities | undefined;
-const { log } = console;
 
 async function saveState() {
     const cache = await caches.open('todo-sw');
@@ -106,6 +105,8 @@ async function handlePostRequest(request: Request, method: string): Promise<Resp
         }
 
         case 'log': {
+            const { log } = console;
+
             switch (data.type) {
                 case 'info': {
                     log('%c[INFO]', 'color: blue; font-weight: 600;', data);
