9eb1caffb8e5bc4c45646519fe3959ef73eea8de
vadim_budarin <budarin-vv@yandex.ru>
2023-06-09 11:04:29 +0300

    fix test

--- src/domain/selectors/__tests__/getNavPanelItemProps.test.tsx
+++ src/domain/selectors/__tests__/getNavPanelItemProps.test.tsx
@@ -6,7 +6,7 @@ import { getNavPanelItemProps } from '../getNavPanelItemProps.ts';
 import './utils/store.setup.ts';
 
 describe('getNavPanelItemProps', () => {
-    it('должен вернуть свойства для указанного элемента панели навигации', () => {
+    it('должен вернуть свойства для элемента панели навигации из списка фильтров', () => {
         let selectedNavItemProps = {} as ReturnType<typeof getNavPanelItemProps>;
 
         act(() => {
@@ -26,6 +26,29 @@ describe('getNavPanelItemProps', () => {
             isCategory: false,
             selected: false,
             icon: 'inbox.png',
+        });
+    });
+
+    it('должен вернуть свойства для элемента панели навигации из списка категорий', () => {
+        let selectedNavItemProps = {} as ReturnType<typeof getNavPanelItemProps>;
+
+        act(() => {
+            createRoot(document.createElement('div')).render(
+                <TestComponent
+                    hook={() => {
+                        selectedNavItemProps = getNavPanelItemProps(1, 'category');
+                    }}
+                />,
+            );
+        });
+
+        expect(selectedNavItemProps).not.toBeUndefined();
+
+        expect(selectedNavItemProps).toEqual({
+            title: 'Работа',
+            isCategory: true,
+            selected: false,
+            icon: 'other.png',
         });
     });
 });
