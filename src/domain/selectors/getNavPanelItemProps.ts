3112ef44224b1ea0add4a142998d7f26cf1e6266
vadim_budarin <budarin-vv@yandex.ru>
2023-06-09 09:17:41 +0300

    fix hook

--- src/domain/selectors/getNavPanelItemProps.ts
+++ src/domain/selectors/getNavPanelItemProps.ts
@@ -12,31 +12,26 @@ import {
 export const getNavPanelItemPropsSelector = (id: NavigationFilterKey, navigationType: NavigationFilterType) =>
     useCallback(
         (state: TodosState) => {
-            let selected = false;
-            let title = '';
-            let icon = '';
-
             const filter = state.navigationFilter;
             const isCategory = navigationFilterTypes.category === navigationType;
 
             if (isCategory) {
                 const category = state.categories.byId[id as Id];
 
-                title = category.category;
-                selected = title === filter.title;
-                icon = state.icons.byId[category.icon_id].icon_name;
+                return {
+                    isCategory,
+                    title: category.category,
+                    selected: category.category === filter.title,
+                    icon: state.icons.byId[category.icon_id].icon_name,
+                };
             } else {
-                title = navigationFilters[id as NavigationFiltersKey];
-                selected = id === filter.key;
-                icon = navigationFilterIcons[id as NavigationFiltersKey];
+                return {
+                    isCategory,
+                    title: navigationFilters[id as NavigationFiltersKey],
+                    selected: id === filter.key,
+                    icon: navigationFilterIcons[id as NavigationFiltersKey],
+                };
             }
-
-            return {
-                isCategory,
-                title,
-                selected,
-                icon,
-            };
         },
         [id, navigationType],
     );
