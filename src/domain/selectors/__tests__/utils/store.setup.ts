5ccd23b69653edda156531eaaf873391f144fa25
vadim_budarin <budarin-vv@yandex.ru>
2023-06-09 09:19:44 +0300

    fix test

--- src/domain/selectors/__tests__/utils/store.setup.ts
+++ src/domain/selectors/__tests__/utils/store.setup.ts
@@ -6,35 +6,19 @@ const { icons, statuses, categories, todos } = serverInitialState;
 const { _addIcon, _addStatus, _addCategory, _addTodo } = useTodoStore.getState();
 
 icons?.forEach((icon) => {
-    try {
-        _addIcon(icon);
-    } catch (error) {
-        console.error(error);
-    }
+    _addIcon(icon);
 });
 
 statuses?.forEach((status) => {
-    try {
-        _addStatus(status);
-    } catch (error) {
-        console.error(error);
-    }
+    _addStatus(status);
 });
 
 categories?.forEach((category) => {
-    try {
-        _addCategory(category);
-    } catch (error) {
-        console.error(error);
-    }
+    _addCategory(category);
 });
 
 todos?.forEach((todo) => {
-    try {
-        _addTodo(todo);
-    } catch (error) {
-        console.error(error);
-    }
+    _addTodo(todo);
 });
 
 const initialState = useTodoStore.getState();
