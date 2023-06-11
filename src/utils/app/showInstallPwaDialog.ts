9551dc9cdd43198237ecbd57c86eda90edee73b0
vadim_budarin <budarin-vv@yandex.ru>
2023-06-12 01:11:07 +0300

    fix

--- src/utils/app/showInstallPwaDialog.ts
+++ src/utils/app/showInstallPwaDialog.ts
@@ -1,18 +1,13 @@
+// @ts-nocheck
 export function showInstallPwaDialog() {
     const pwaInstall = document.getElementsByTagName('pwa-install')[0];
-    // @ts-ignore
+
     if (pwaInstall && pwaInstall.isInstallAvailable) {
-        // @ts-ignore
         if (pwaInstall.isAppleMobilePlatform) {
-            // @ts-ignore
             pwaInstall.isAppleMobilePlatform = true;
-            // @ts-ignore
             pwaInstall.hideDialog();
-            // @ts-ignore
             pwaInstall.showDialog(true);
-            return;
         } else {
-            // @ts-ignore
             pwaInstall.showDialog();
         }
     }
