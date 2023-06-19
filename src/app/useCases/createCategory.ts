29d2307fa2b2193d2fdd6e9799effed7508dcd34
vadim_budarin <budarin-vv@yandex.ru>
2023-06-20 01:12:06 +0300

    fix code

--- src/app/useCases/createCategory.ts
+++ src/app/useCases/createCategory.ts
@@ -3,6 +3,7 @@ import { useLogger } from '../../services/adapters/useLogger.ts';
 import { useNotification } from '../../services/adapters/useNotification.ts';
 
 import { useTodoStore } from '../../domain/store/store.tsx';
+import { validateNewCategory } from '../../domain/entities/category/index.ts';
 import { createCategoryNavFilter } from '../../domain/store/navigationFilter/createCategoryNavFilter.ts';
 
 const api = useApi();
@@ -11,21 +12,40 @@ const notification = useNotification();
 const NOT_CATEGORY_OBJECT = 'Объект не является описанием Категории';
 
 export async function createCategory(
-    category: NewCategory,
+    category: UnknownObject,
     isMountedRef: React.MutableRefObject<boolean>,
 ): Promise<void> {
     if (!isMountedRef.current) {
         return;
     }
 
+    const { entity, error: validateError } = validateNewCategory(category);
+
+    if (validateError) {
+        notification.notifyError(`Ошибка: ${validateError}`, {
+            toastId: 'create_category_error' + category.category,
+        });
+
+        logger.error(validateError);
+    }
+
+    if (!entity) {
+        notification.notifyError(NOT_CATEGORY_OBJECT, {
+            toastId: 'create_category_error' + NOT_CATEGORY_OBJECT,
+        });
+
+        logger.error(NOT_CATEGORY_OBJECT);
+        return;
+    }
+
     try {
-        const { result, error } = await api.createCategory(category);
+        const { result, error } = await api.createCategory(entity);
 
         if (error) {
             notification.notifyError(
-                `Ошибка: Не удалось создать категорию ${category.category}. Попробуйте позже еще раз.`,
+                `Ошибка: Не удалось создать категорию ${entity.category}. Попробуйте позже еще раз.`,
                 {
-                    toastId: 'create_category_error' + category.category,
+                    toastId: 'create_category_error' + entity.category,
                 },
             );
 
@@ -41,10 +61,10 @@ export async function createCategory(
         store._addCategory(result);
 
         // устанавливаем навигационный фильтр на данную категорию
-        store.setNavigationFilter(createCategoryNavFilter(result.category_id, result.category));
+        store.setNavigationFilter(createCategoryNavFilter(result.category_id, entity.category));
     } catch (error) {
         notification.notifyError(`Ошибка: ${error}`, {
-            toastId: 'create_category_error' + category.category,
+            toastId: 'create_category_error' + entity.category,
         });
 
         logger.error((error as Error).message);
