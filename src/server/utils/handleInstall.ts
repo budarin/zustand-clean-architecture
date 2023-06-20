6f87d755f80684731b2fe36495d6bd289e8d9baa
vadim_budarin <budarin-vv@yandex.ru>
2023-06-20 04:50:05 +0300

    fix code

--- src/server/utils/handleInstall.ts
+++ src/server/utils/handleInstall.ts
@@ -1,4 +1,4 @@
-import { self } from '../index.ts';
+declare var self: ServiceWorkerGlobalScope & typeof globalThis & { VERSION: string };
 
 export function handleInstall(event: ExtendableEvent): void {
     self.skipWaiting();
