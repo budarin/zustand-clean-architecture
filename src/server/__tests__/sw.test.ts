55f97d37144e6f433f683f37d8b1f323fb52d393
Vadim Budarin <budarin-vv@yandex.ru>
2023-06-22 02:56:51 +0300

    fix

--- src/server/__tests__/sw.test.ts
+++ src/server/__tests__/sw.test.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
     await page.goto('http://127.0.0.1:3000/');
 
-    // Expect a title "to contain" a substring..
+    // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Мои Задачи/);
 });
