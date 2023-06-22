b097ab26ee07341b696708ad461722fb4c7fdd25
Vadim Budarin <budarin-vv@yandex.ru>
2023-06-22 21:45:13 +0300

    fix code

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,8 @@
 import { initApp } from './utils/app/initApp.tsx';
-import { useLogger } from './services/adapters/useLogger.ts';
-import { setUpPwaInstall } from './utils/pwa-install/setUpPwaInstall.ts';
+import { setUpPwaInstall } from './utils/service-worker/pwa-install/setUpPwaInstall.ts';
+import { handleSetupServiceWorker } from './utils/service-worker/handleSetupServiceWorker.ts';
 
-const logger = useLogger();
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready
-        .then((registration) => {
-            if (registration.active?.state === 'activated') {
-                initApp();
-            } else {
-                registration.active?.addEventListener('statechange', initApp);
-            }
-        })
-        .catch((error) => {
-            logger.error({ message: 'Ошибка регистрации service worker', error });
-        });
-} else {
-    logger.error('Устройство не поддерживает service worker');
-}
+handleSetupServiceWorker().then(initApp);
 
 if ('BeforeInstallPromptEvent' in window) {
     window.addEventListener('beforeinstallprompt', setUpPwaInstall);
