1324355150d3ca1e481a4b2c09348b9e5dc2e5c9
vadim_budarin <budarin-vv@yandex.ru>
2023-06-30 03:49:42 +0300

    fix styles

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
-    plugins: ['stylelint-use-logical'],
+    plugins: ['stylelint-use-logical-spec'],
     rules: {
         'selector-class-pattern': null,
         'custom-property-pattern': null,
+        'liberty/use-logical-spec': 'always',
     },
 };
