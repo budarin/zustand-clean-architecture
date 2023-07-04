d5383b2dd788c25f116ef880e96c43b4d15b2c27
Vadim Budarin <budarin-vv@yandex.ru>
2023-07-04 13:22:57 +0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,27 @@ module.exports = {
         'plugin:import/recommended',
         'plugin:@typescript-eslint/recommended',
     ],
+
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'jsx-a11y',
+        'optimize-regex',
+        'jest',
+        'jest-dom',
+        'testing-library',
+        '@typescript-eslint',
+        '@babel/eslint-plugin',
+        'react',
+        // должен быть последним!
+        'prettier',
+    ],
+    rules: {
+        'react/prop-types': 'off',
+    },
     overrides: [
         {
             env: {
@@ -19,14 +40,10 @@ module.exports = {
                 sourceType: 'script',
             },
         },
+        {
+            // только для тестов используем плагины для тестов
+            files: '**/?(*.)+(spec|test).(js|ts|tsx)',
+            extends: ['plugin:jest/all', 'plugin:jest-dom/recommended', 'plugin:testing-library/all'],
+        },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint', '@babel/eslint-plugin', 'react', 'jsx-a11y', 'optimize-regex'],
-    rules: {
-        'react/prop-types': 'off',
-    },
 };
--- documentation/eslint.md
+++ documentation/eslint.md
@@ -2,3 +2,6 @@
 
 Посмотреть какие правила сейчас применяются ESLint можно при помощи команды
 `eslint --print-config .eslintrc.json > result.json`
+
+узнать топ 10 самых медленных правил
+`timing=1 npm run lint`
